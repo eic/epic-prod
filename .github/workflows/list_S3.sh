0fe4f3be4fad0eae8b070832bb3c35045a823614
Sakib Rahman <rahmans@myumanitoba.ca>
2023-05-31 18:27:14 -0500

    Create list of files in yml format

--- .github/workflows/list_S3.sh
+++ .github/workflows/list_S3.sh
@@ -5,5 +5,5 @@ OBJECTS=$(./mc ls --recursive S3/eictest/EPIC/EVGEN)
 
 # Iterate over the objects and print their names
 while IFS= read -r object; do
-  echo "$object"
+  echo "- $object"
 done <<< "$OBJECTS"
