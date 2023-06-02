5a19b1fcb3ba0c283464a3c9225061de5b0e7a1e
Sakib Rahman <rahmans@myumanitoba.ca>
2023-06-02 14:15:32 -0500

    Strip out the date and time block from the file name

--- docs/_data/list_S3.sh
+++ docs/_data/list_S3.sh
@@ -5,5 +5,6 @@ OBJECTS=$(mc ls --recursive S3/eictest/EPIC/EVGEN)
 
 # Iterate over the objects and print their names
 while IFS= read -r object; do
-  echo "- $object"
+  result="${object#*] }"
+  echo "- $result"
 done <<< "$OBJECTS"
