2718571e1d4db92d9b80f2b119f6dc3de996a145
Sakib Rahman <rahmans@myumanitoba.ca>
2023-06-02 14:39:08 -0500

    Only keep the name of the file and strip all other information

--- docs/_data/list_S3.sh
+++ docs/_data/list_S3.sh
@@ -5,6 +5,6 @@ OBJECTS=$(mc ls --recursive S3/eictest/EPIC/EVGEN)
 
 # Iterate over the objects and print their names
 while IFS= read -r object; do
-  result="${object#*] }"
-  echo "- $result"
+  name_only=$(echo "$object" | grep -oP 'STANDARD \K.*')
+  echo "- $name_only"
 done <<< "$OBJECTS"
