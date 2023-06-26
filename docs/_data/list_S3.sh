fc843447ead1030959ac95f6309b3c504e753ef5
Alex Hayes <76755531+ahayesgithub@users.noreply.github.com>
2023-06-26 11:07:04 -0500

    Added braces to variable ${name_only}
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- docs/_data/list_S3.sh
+++ docs/_data/list_S3.sh
@@ -6,8 +6,8 @@ OBJECTS=$(mc ls S3/eictest/EPIC/LOG)
 # Iterate over the objects: print their names and create folders in the repository containing index.md
 while IFS= read -r object; do
   name_only= $object | sed -n 's/^\[[^][]*][[:blank:]]*[^[:blank:]]*[[:blank:]]*\(.*\)$/\1/p' # extracts the folder name from mc ls
-  mkdir -p ./docs/_data/LOG/"$name_only"
-  touch ./docs/_data/LOG/"$name_only"index.md
-  mc tree S3/eictest/EPIC/LOG/"$name_only" > ./docs/_data/LOG/"$name_only"index.md
+  mkdir -p ./docs/_data/LOG/"${name_only}"
+  touch ./docs/_data/LOG/"${name_only}"index.md
+  mc tree S3/eictest/EPIC/LOG/"${name_only}" > ./docs/_data/LOG/"${name_only}"index.md
   echo -e "- text: "$object"\n  url: "/$name_only""
 done <<< "$OBJECTS"
