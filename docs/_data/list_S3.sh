4aae2cfe85ff252021037250c8a1177f853c8d12
Alex Hayes <76755531+ahayesgithub@users.noreply.github.com>
2023-06-28 15:14:29 -0500

    Added command substitution to fix name extraction error

--- docs/_data/list_S3.sh
+++ docs/_data/list_S3.sh
@@ -5,7 +5,7 @@ OBJECTS=$(mc ls S3/eictest/EPIC/LOG)
 
 # Iterate over the objects: print their names and create folders in the repository containing index.md
 while IFS= read -r object; do
-  name_only= $object | sed -n 's/^\[[^][]*][[:blank:]]*[^[:blank:]]*[[:blank:]]*\(.*\)$/\1/p' # extracts the folder name from mc ls
+  name_only=$(echo "$object" | sed -n 's/^\[[^][]*][[:blank:]]*[^[:blank:]]*[[:blank:]]*\(.*\)$/\1/p') # extracts the folder name from mc ls
   mkdir -p ./docs/_data/LOG/"${name_only}"
   touch ./docs/_data/LOG/"${name_only}"index.md
   mc tree S3/eictest/EPIC/LOG/"${name_only}" > ./docs/_data/LOG/"${name_only}"index.md
