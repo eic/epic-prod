d04abc6e40aaa73a5ed78c9eba1e4ef44bec654e
Alex Hayes <76755531+ahayesgithub@users.noreply.github.com>
2023-08-21 14:20:02 -0500

    Added code block indicators to the start and end of every index.md

--- docs/_data/list_S3.sh
+++ docs/_data/list_S3.sh
@@ -8,6 +8,8 @@ while IFS= read -r object; do
   name_only=$(echo "$object" | sed -n 's/^\[[^][]*][[:blank:]]*[^[:blank:]]*[[:blank:]]*\(.*\)$/\1/p') # extracts the folder name from mc ls
   mkdir -p ./docs/LOG/"${name_only}"
   touch ./docs/LOG/"${name_only}"index.md
-  mc tree S3/eictest/EPIC/LOG/"${name_only}" > ./docs/LOG/"${name_only}"index.md
+  echo '```' > ./docs/LOG/"${name_only}"index.md
+  mc tree S3/eictest/EPIC/LOG/"${name_only}" >> ./docs/LOG/"${name_only}"index.md
+  echo '```' >> ./docs/LOG/"${name_only}"index.md
   echo -e "- text: "$name_only"\n  url: "/epic-prod/LOG/$name_only""
 done <<< "$OBJECTS"
