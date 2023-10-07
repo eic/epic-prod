049ce0714ae1f45ca4edbc9fc7312db812267f80
Alex Hayes <76755531+ahayesgithub@users.noreply.github.com>
2023-10-06 21:04:39 -0500

    Updated file counting script to recognize correct extension (#44)
    * Updated file counting script to recognize correct extension
    
    * Use RECO directory instead of LOG
    
    * Replace references of LOG with RECO
    
    * Remove the LOG directory
    
    ---------
    
    Co-authored-by: Sakib Rahman <rahmans@myumanitoba.ca>

--- docs/_data/list_S3.sh
+++ docs/_data/list_S3.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# List all objects in the S3 LOG directory
-OBJECTS=$(mc ls S3/eictest/EPIC/LOG)
+# List all objects in the S3 RECO directory
+OBJECTS=$(mc ls S3/eictest/EPIC/RECO)
 
 # Iterate over the objects: print their names and create folders in the repository containing index.md
 while IFS= read -r object; do
   name_only=$(echo "$object" | sed -n 's/^\[[^][]*][[:blank:]]*[^[:blank:]]*[[:blank:]]*\(.*\)$/\1/p') # extracts the folder name from mc ls
-  mkdir -p ./docs/LOG/"${name_only}"
-  touch ./docs/LOG/"${name_only}"index.md
-  echo '```' > ./docs/LOG/"${name_only}"index.md
-  mc tree --files S3/eictest/EPIC/LOG/"${name_only}" | sed 's/.*\.root$//g' | uniq -c | sed 's/.*1 //' >> ./docs/LOG/"${name_only}"index.md # contructs tree diagram with count of files in each directory
-  echo '```' >> ./docs/LOG/"${name_only}"index.md
-  echo -e "- text: "$name_only"\n  url: "/epic-prod/LOG/$name_only""
+  mkdir -p ./docs/RECO/"${name_only}"
+  touch ./docs/RECO/"${name_only}"index.md
+  echo '```' > ./docs/RECO/"${name_only}"index.md
+  mc tree --files S3/eictest/EPIC/RECO/"${name_only}" | sed 's/.*\.root$//g' | uniq -c | sed 's/.*1 //' >> ./docs/RECO/"${name_only}"index.md # contructs tree diagram with count of files in each directory
+  echo '```' >> ./docs/RECO/"${name_only}"index.md
+  echo -e "- text: "$name_only"\n  url: "/epic-prod/RECO/$name_only""
 done <<< "$OBJECTS"
