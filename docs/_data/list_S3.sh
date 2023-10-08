eea3263eb02d7d11209020b81502fb88a0fc9612
Sakib Rahman <rahmans@myumanitoba.ca>
2023-10-08 17:15:33 -0500

    Use an argument instead of explicitly calling RECO or FULL

--- docs/_data/list_S3.sh
+++ docs/_data/list_S3.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# List all objects in the S3 RECO directory
-OBJECTS=$(mc ls S3/eictest/EPIC/RECO)
+# List all objects in the RECO or FULL directory
+OBJECTS=$(mc ls S3/eictest/EPIC/$1)
 
 # Iterate over the objects: print their names and create folders in the repository containing index.md
 while IFS= read -r object; do
   name_only=$(echo "$object" | sed -n 's/^\[[^][]*][[:blank:]]*[^[:blank:]]*[[:blank:]]*\(.*\)$/\1/p') # extracts the folder name from mc ls
-  mkdir -p ./docs/RECO/"${name_only}"
-  touch ./docs/RECO/"${name_only}"index.md
-  echo '```' > ./docs/RECO/"${name_only}"index.md
-  mc tree --files S3/eictest/EPIC/RECO/"${name_only}" | sed 's/.*\.root$//g' | uniq -c | sed 's/.*1 //' >> ./docs/RECO/"${name_only}"index.md # contructs tree diagram with count of files in each directory
-  echo '```' >> ./docs/RECO/"${name_only}"index.md
-  echo -e "- text: "$name_only"\n  url: "/epic-prod/RECO/$name_only""
+  mkdir -p ./docs/$1/"${name_only}"
+  touch ./docs/$1/"${name_only}"index.md
+  echo '```' > ./docs/$1/"${name_only}"index.md
+  mc tree --files S3/eictest/EPIC/$1/"${name_only}" | sed 's/.*\.root$//g' | uniq -c | sed 's/.*1 //' >> ./docs/$1/"${name_only}"index.md # contructs tree diagram with count of files in each directory
+  echo '```' >> ./docs/$1/"${name_only}"index.md
+  echo -e "- text: "$name_only"\n  url: "/epic-prod/$1/$name_only""
 done <<< "$OBJECTS"
